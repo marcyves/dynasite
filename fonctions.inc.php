a72aba8fccc96024dc82ef0e3887b53e696af939
Marc Yves <m.augier@me.com>
2017-11-23 09:51:41 +0100

    mise à niveau decodeTemplate
    Utilisation de fonctions pour chaque étiquette.
    Prise en compte d'un .htaccess qui permet d'utiliser des liens du style http://site.com/dynasite/nom-page

--- fonctions.inc.php
+++ fonctions.inc.php
@@ -8,29 +8,46 @@ function prepareMenu($dossier_menu, $page_active){
 	foreach($fichiers as $fichier){
 		$ext  = substr($fichier, strrpos($fichier,'.')+1);
 		if($ext == 'menu'){
+			// Nom de la page avec numéro
 			$page  = substr($fichier, 0, strrpos($fichier, '.'));
+			// Nom de la page sans numéro
 			$label = substr($fichier, 1, strrpos($fichier, '.')-1);
-			if ($page == $page_active){
-//				$tmp  .= "<li class='active'><a href='index.php?page=$page'>$label</a></li>";
-				$tmp  .= "<li class='active'><a href='page-$page.html'>$label</a></li>";
+			if ($label == $page_active){
+//				$tmp .= "<li class='active'><a href='index.php?page=$page'>$label</a></li>";
+				$tmp .= "<li class='active'><a href='$label'>$label</a></li>";
 			} else {
-//				$tmp .= "<li><a href='index.php?page=$page'>$label</a></li>";
-				$tmp  .= "<li><a href='page-$page.html'>$label</a></li>";
+//				$tmp .= "<li><a href='index.php?page=$page'>$label</a></li>";	
+				$tmp .= "<li><a href='$label'>$label</a></li>";
 			}
 		}
 	}
 	$tmp .= '</ul>';
-
+	
 	return $tmp;
 }
 
 function prepareContenu($dossier_menu, $page_active){
 	// Extraction du contenu
-	$fichier_contenu = $dossier_menu."/$page_active.menu";
-
-	if(is_readable($fichier_contenu)){
-		$tmp = file_get_contents($fichier_contenu);
-	} else {
+	$boucle = true;
+	$trouve = false;
+	$i =0;
+	while($boucle){
+		// construit le chemin et le nom du fichier avec un numéro $i
+		$fichier_contenu = $dossier_menu."/$i$page_active.menu";
+		if(is_readable($fichier_contenu)){
+			// Le fichier est trouvé
+			// On lit le contenu
+			$tmp = file_get_contents($fichier_contenu);
+			// On positionne les interrupteurs logiques
+			$boucle = false;
+			$trouve = true;
+		}
+		$i++;
+		if($i>9){
+			$boucle =false;
+		}
+	}
+	if (!$trouve){
 		$tmp = "<h1>La page demandée n'existe pas</h1>";
 	}
 	return $tmp;
@@ -39,25 +56,25 @@ function prepareContenu($dossier_menu, $page_active){
 function prepareSocial($dossier_social){
 	// Extraction Social
 	$tmp = '<ul class="icons">';
-
+	
 	// Obtenir la liste des fichiers contenus dans le dossier $dossier_social
 	$fichiers = scandir( $dossier_social , SCANDIR_SORT_ASCENDING );
-
+	
 	// Boucle pour chaque fichier trouvé dans le dossier $dossier_social
 	foreach($fichiers as $fichier){
 		/*
 		Pour obtenir l'extension, on coupe la chaine $fichier à partir du caractère après le point jusqu'à la fin.
-
+		
 		substr  ( string $string , int $start [, int $length ] )
-			Retourne le segment de string défini par start et length.
-
+			Retourne le segment de string défini par start et length. 
+		
 		strrpos ( string $haystack , string $needle [, int $offset = 0 ] )
 			Cherche la position numérique de la dernière occurrence de needle dans la chaîne haystack.
 		*/
 		$ext  = substr($fichier, strrpos($fichier,'.')+1);
 		// On ne traite que les fichiers dont l'extension est 'social'
 		if($ext == 'social'){
-			/*
+			/* 
 			On veut extraire le nom du fichier pour obtenir un label en minuscules
 			On extrait cette fois avec substr depuis le début (0) jusqu'à la position du point.
 			*/
@@ -69,7 +86,7 @@ function prepareSocial($dossier_social){
 		}
 	}
 	$tmp .= '</ul>';
-
+	
 	return $tmp;
 }
 
@@ -80,19 +97,19 @@ function decodeTemplate($template, $etiquette, $page_active){
 
 	$etiquette['menu']    = prepareMenu($dossier_menu, $page_active);
 	$etiquette['contenu'] = prepareContenu($dossier_menu, $page_active);
-	$etiquette['social']  = prepareSocial($dossier_social);
-
+	$etiquette['social'] = prepareSocial($dossier_social);
+					
 	// Traitement du template
-	if (file_exists($dossier_template.$fichier_template)){
-		$page_html = file_get_contents($dossier_template . $fichier_template, FILE_USE_INCLUDE_PATH);
-
+	if (file_exists($dossier_template."/".$fichier_template)){
+		$page_html = file_get_contents($dossier_template ."/". $fichier_template, FILE_USE_INCLUDE_PATH);
+		
 		foreach($etiquette as $nom => $valeur){
 			$page_html = str_replace("@@$nom@@" , $valeur , $page_html);
 		}
 	} else {
-		$page_html = "<html><header><title>ERREUR</title></header><body><h1>ERREUR TEMPLATE</h1>Le fichier $dossier_template.$fichier_template n'existe pas</body></html>";
+		$page_html = "<html><header><title>ERREUR</title></header><body><h1>ERREUR TEMPLATE</h1>Le fichier $dossier_template/$fichier_template n'existe pas</body></html>";
 	}
-	return $page_html;
+	return $page_html;	
 }
 
 ?>
